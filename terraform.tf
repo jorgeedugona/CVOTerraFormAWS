298b0b268a0556c6fa80cbd402964fa3263f8514
jorgeedugona <gjorge@netapp.com>
2021-03-14 08:46:00 +0000

    Add files via upload

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+variable "region" {
+	type = string
+}
+
+variable "vpc_id" {
+	type = string
+}
+
+variable "subnet_id" {
+	type = string
+}
+
+variable "token" {
+	type = string
+}
+
+variable "publicIP" {
+	type = bool
+}
