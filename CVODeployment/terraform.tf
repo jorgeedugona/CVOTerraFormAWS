12b5adf841c06550f9dee11b006f1426d05d3f0e
jorgeedugona <gjorge@netapp.com>
2021-04-06 15:43:16 +0100

    Create terraform.tf

---
+++ CVODeployment/terraform.tf
@@ -0,0 +1,79 @@
+variable "region" {
+	type = string
+}
+
+variable "vpc_id" {
+	type = string
+}
+
+variable "token" {
+	type = string
+}
+
+variable "ha" {
+	type = bool
+}
+
+variable "instance_type" {
+	type = string
+}
+
+variable "password" {
+	type = string
+}
+
+variable "client_id" {
+	type = string
+}
+
+variable "subnet_id" {
+	type = string
+}
+
+variable "license_single_node" {
+	type = string
+}
+
+variable "node1_subnet_id" {
+	type = string
+}
+
+variable "node2_subnet_id" {
+	type = string
+}
+
+variable "mediator_subnet_id" {
+	type = string
+}
+
+variable "floating_ip_1" {
+	type = string
+}
+
+variable "floating_ip_2" {
+	type = string
+}
+
+variable "floating_ip_3" {
+	type = string
+}
+
+variable "floating_ip_4" {
+	type = string
+}
+
+variable "license_node_1" {
+	type = string
+}
+
+variable "license_node_2" {
+	type = string
+}
+
+variable "route_table_id1" {
+	type = string
+}
+
+variable "route_table_id2" {
+	type = string
+}
